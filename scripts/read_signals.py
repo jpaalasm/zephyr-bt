f80bdccb40501c17eb18d3e061c16b8dd9765b56
Joonas Paalasmaa <joonas.paalasmaa@gmail.com>
2012-01-14 12:54:07 +0200

    Started a signal reception class. Enabled ECG signal reception.

--- scripts/read_signals.py
+++ scripts/read_signals.py
@@ -5,15 +5,36 @@ import zephyr.message
 import zephyr.connection
 
 
+class SignalReceiver:
+    def __init__(self):
+        pass
+    
+    def handle_message(self, message):
+        print "Message: %02x" % message.message_id
+        
+        if message.message_id == 0x21:
+            self.handle_breathing_payload(message.payload)
+    
+    def handle_breathing_payload(self, payload):
+        assert len(payload) == 32
+        
+        header = payload[:9]
+        signal_bytes = payload[9:]
+        
+        print "Breathing data:", payload
+
+
 def callback(message):
     print "Message: type %02x, payload %s, eom %s" % (message.message_id, message.payload, message.eom)
 
 def main():
-    ser = serial.Serial(23, timeout=0.1)
+    signal_receiver = SignalReceiver()
     
-    connection = zephyr.connection.Connection(ser, callback)
+    ser = serial.Serial(23, timeout=0.1)
+    connection = zephyr.connection.Connection(ser, signal_receiver.handle_message)
     
     connection.send_message(0x15, [1])
+    connection.send_message(0x16, [1])
     connection.send_message(0x19, [1])
     connection.send_message(0x1E, [1])
     
