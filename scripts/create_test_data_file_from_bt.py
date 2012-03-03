6971e56755f18576b735191e726676ad8c268cac
Joonas Paalasmaa <joonas.paalasmaa@gmail.com>
2012-03-03 16:51:47 +0200

    Refactored test data file creation.

--- scripts/create_test_data_file_from_bt.py
+++ scripts/create_test_data_file_from_bt.py
@@ -14,40 +14,11 @@ def main():
     serial_port = serial_port_dict[platform.system()]
     ser = serial.Serial(serial_port)
     
-    protocol = zephyr.protocol.Protocol(ser, None)
-    
+    callback = lambda x: None
+    protocol = zephyr.protocol.Protocol(ser, callback, "../test_data/120-second-bt-stream")
     protocol.enable_signals()
     
-    start_time = time.time()
-    
-    recording_time = 120.0
-    
-    chunks_timing = []
-    
-    data_file = file("../test_data/120-second-bt-stream.dat", "wb")
-    timing_file = file("../test_data/120-second-bt-stream-timing.csv", "wb")
-    timing_file_csv_writer = csv.writer(timing_file)
-    
-    while True:
-        time_before = time.time()
-        byte = ser.read(1)
-        delay = time.time() - time_before
-        
-        relative_previous_chunk_time = time_before - start_time
-        
-        data_file_position = data_file.tell()
-        
-        if delay > 0.01 and data_file_position:
-            timing_file_csv_writer.writerow((relative_previous_chunk_time, data_file_position))
-            print relative_previous_chunk_time, data_file_position
-        
-        if relative_previous_chunk_time > recording_time:
-            break
-        
-        data_file.write(byte)
-    
-    data_file.close()
-    timing_file.close()
+    protocol.read_and_handle_forever()
 
 
 if __name__ == "__main__":
--- src/zephyr/protocol.py
+++ src/zephyr/protocol.py
@@ -1,13 +1,51 @@
 
+import os
+import time
+import csv
 import logging
 
 import zephyr.message
 
 
+class MessageDataLogger:
+    def __init__(self, log_file_basepath):
+        self.data_file = file(log_file_basepath + ".dat", "wb")
+        self.timing_file = file(log_file_basepath + "-timing.csv", "wb")
+        self.timing_file_csv_writer = csv.writer(self.timing_file)
+        
+        self.start_time = None
+        self.time_before = None
+    
+    def __call__(self, bytes):
+        if self.start_time is None:
+            now = time.time()
+            self.start_time = now
+            self.time_before = now
+        
+        delay = time.time() - self.time_before
+        
+        relative_previous_chunk_time = self.time_before - self.start_time
+        
+        data_file_position = self.data_file.tell()
+        
+        if delay > 0.01 and data_file_position:
+            self.timing_file_csv_writer.writerow((relative_previous_chunk_time, data_file_position))
+            print relative_previous_chunk_time, data_file_position
+        
+        self.data_file.write(bytes)
+        
+        self.time_before = time.time()
+
+
 class Protocol:
-    def __init__(self, connection, callback):
+    def __init__(self, connection, callback, log_file_basepath=None):
         self.connection = connection
         self.message_parser = zephyr.message.MessageFrameParser(callback)
+        
+        if log_file_basepath is not None:
+            self.message_logger = MessageDataLogger(log_file_basepath)
+        else:
+            self.message_logger = lambda x: None
     
     def send_message(self, message_id, payload):
         message_frame = zephyr.message.create_message_frame(message_id, payload)
@@ -16,14 +54,13 @@ class Protocol:
     def read_and_handle_bytes(self, num_bytes):
         data_string = self.connection.read(num_bytes)
         self.message_parser.parse_data(data_string)
-        
-        nbytes_read = len(data_string)
-        return nbytes_read
+        return data_string
     
     def read_and_handle_forever(self):
         try:
             while True:
-                self.read_and_handle_bytes(1)
+                bytes = self.read_and_handle_bytes(1)
+                self.message_logger(bytes)
         except KeyboardInterrupt:
             logging.info("Received Ctrl-C, exiting")
     
@@ -32,3 +69,4 @@ class Protocol:
         self.send_message(0x16, [1])
         self.send_message(0x19, [1])
         self.send_message(0x1E, [1])
+    
