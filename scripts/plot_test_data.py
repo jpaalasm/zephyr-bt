402e4c7f134bd842a4e17daf313ac30895ffd236
Joonas Paalasmaa <joonas.paalasmaa@gmail.com>
2012-03-17 14:52:37 +0200

    Typo.

--- scripts/plot_test_data.py
+++ scripts/plot_test_data.py
@@ -6,7 +6,7 @@ import zephyr.testing
 
 def main():
     signal_collector = zephyr.rr_event.SignalCollectorWithRRProcessing(False)
-    zephyr.testing.simulate_signal_packets_from_file("../test_data/120-second-bt-stream.dat", "../test_data/120-second-bt-stream-timing.json", signal_collector.handle_packet, False)
+    zephyr.testing.simulate_signal_packets_from_file("../test_data/120-second-bt-stream.dat", "../test_data/120-second-bt-stream-timing.csv", signal_collector.handle_packet, False)
     zephyr.testing.visualize_measurements(signal_collector)
 
 if __name__ == "__main__":
