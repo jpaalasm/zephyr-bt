90e5bdc3f4570cfddd11388ac93f61434121226a
Joonas Paalasmaa <joonas.paalasmaa@gmail.com>
2012-06-02 21:22:52 +0300

    Fix the visualization window to work with all toolkits

--- src/zephyr/visualization.py
+++ src/zephyr/visualization.py
@@ -3,6 +3,7 @@ import numpy
 import time
 import threading
 import matplotlib.pyplot
+from matplotlib.animation import FuncAnimation
 
 class VisualizationWindow:
     def __init__(self, signal_collector):
@@ -23,6 +24,8 @@ class VisualizationWindow:
         self.heartbeat_interval_line = create_empty_line(5, "+")
         self.activity_line = create_empty_line(6, "+")
         
+        self.artists = self.acceleration_lines + [self.breathing_line, self.ecg_line, self.respiration_rate_line, self.heart_rate_line, self.heartbeat_interval_line, self.activity_line]
+        
         self.axes[0].set_ylim((-4, 4))
         self.axes[1].set_ylim((-1000, 1000))
         self.axes[2].set_ylim((-500, 500))
@@ -30,62 +33,55 @@ class VisualizationWindow:
         self.axes[4].set_ylim((0, 120))
         self.axes[5].set_ylim((0, 2))
         self.axes[6].set_ylim((0, 2))
-        
-        self.stop_updating_requested = False
     
-    def update_plots(self):
-        while not self.stop_updating_requested:
-            for stream_name, stream in self.signal_collector.iterate_signal_streams():
-                signal_value_array = numpy.array(stream.samples, dtype=float)
-                
-                x_values = numpy.arange(len(signal_value_array), dtype=float)
-                x_values /= stream.samplerate
-                x_values += stream.end_timestamp - len(signal_value_array) / stream.samplerate
-                
-                if stream_name == "acceleration":
-                    for line_i, line in enumerate(self.acceleration_lines):
-                        line.set_xdata(x_values)
-                        line.set_ydata(signal_value_array[:, line_i])
-                
-                elif stream_name == "breathing":
-                    self.breathing_line.set_xdata(x_values)
-                    self.breathing_line.set_ydata(signal_value_array)
-                
-                elif stream_name == "ecg":
-                    self.ecg_line.set_xdata(x_values)
-                    self.ecg_line.set_ydata(signal_value_array)
+    def update_plots(self, framedata):
+        for stream_name, stream in self.signal_collector.iterate_signal_streams():
+            signal_value_array = numpy.array(stream.samples, dtype=float)
+            
+            x_values = numpy.arange(len(signal_value_array), dtype=float)
+            x_values /= stream.samplerate
+            x_values += stream.end_timestamp - len(signal_value_array) / stream.samplerate
+            
+            if stream_name == "acceleration":
+                for line_i, line in enumerate(self.acceleration_lines):
+                    line.set_xdata(x_values)
+                    line.set_ydata(signal_value_array[:, line_i])
+            
+            elif stream_name == "breathing":
+                self.breathing_line.set_xdata(x_values)
+                self.breathing_line.set_ydata(signal_value_array)
+            
+            elif stream_name == "ecg":
+                self.ecg_line.set_xdata(x_values)
+                self.ecg_line.set_ydata(signal_value_array)
+        
+        
+        for stream_name, event_list in self.signal_collector.iterate_event_streams():
+            if len(event_list) == 0:
+                continue
+            
+            event_data_array = numpy.array(event_list, dtype=float)
+            
+            event_timestamps = event_data_array[:, 0]
+            event_values = event_data_array[:, 1]
             
             
-            for stream_name, event_list in self.signal_collector.iterate_event_streams():
-                if len(event_list) == 0:
-                    continue
-                
-                event_data_array = numpy.array(event_list, dtype=float)
-                
-                event_timestamps = event_data_array[:, 0]
-                event_values = event_data_array[:, 1]
-                
-                
-                event_line_object_map = {"heart_rate": self.heart_rate_line,
-                                         "respiration_rate": self.respiration_rate_line,
-                                         "heartbeat_interval": self.heartbeat_interval_line,
-                                         "activity": self.activity_line}
-                
-                event_line_object = event_line_object_map[stream_name]
-                
-                if event_line_object is not None:
-                    event_line_object.set_xdata(event_timestamps)
-                    event_line_object.set_ydata(event_values)
+            event_line_object_map = {"heart_rate": self.heart_rate_line,
+                                     "respiration_rate": self.respiration_rate_line,
+                                     "heartbeat_interval": self.heartbeat_interval_line,
+                                     "activity": self.activity_line}
             
-            now = time.time()
-            self.axes[0].set_xlim((now - 115, now + 5))
+            event_line_object = event_line_object_map[stream_name]
             
-            matplotlib.pyplot.draw()
-            time.sleep(1.0)
+            if event_line_object is not None:
+                event_line_object.set_xdata(event_timestamps)
+                event_line_object.set_ydata(event_values)
+        
+        now = time.time()
+        self.axes[0].set_xlim((now - 115, now + 5))
+        
+        return self.artists
     
     def show(self):
-        threading.Thread(target=self.update_plots).start()
-        
+        anim = FuncAnimation(self.figure, self.update_plots, interval=1000, blit=False)
         matplotlib.pyplot.show()
-        
-        self.stop_updating_requested = True
