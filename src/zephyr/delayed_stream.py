441cccc340afeb205da75762ce6e145215a858b3
Joonas Paalasmaa <joonas.paalasmaa@gmail.com>
2012-09-23 21:56:49 +0300

    Split delay configuration into default_delay and specific_delays

--- src/zephyr/delayed_stream.py
+++ src/zephyr/delayed_stream.py
@@ -7,11 +7,12 @@ import time
 import zephyr
 
 class DelayedRealTimeStream(threading.Thread):
-    def __init__(self, signal_collector, callbacks, delay):
+    def __init__(self, signal_collector, callbacks, default_delay, specific_delays={}):
         threading.Thread.__init__(self)
         self.signal_collector = signal_collector
         self.callbacks = callbacks
-        self.delay = delay
+        self.default_delay = default_delay
+        self.specific_delays = specific_delays
         
         self.stream_output_positions = collections.defaultdict(lambda: 0)
         
@@ -25,12 +26,15 @@ class DelayedRealTimeStream(threading.Thread):
     
     def run(self):
         while not self.terminate_requested:
-            delayed_current_time = zephyr.time() - self.delay
-            
+            now = zephyr.time()
             all_streams = itertools.chain(self.signal_collector.iterate_signal_stream_histories(),
                                           self.signal_collector.iterate_event_streams())
             
             for signal_stream_name, signal_stream_history in all_streams:
+                delay = self.specific_delays.get(signal_stream_name, self.default_delay)
+                
+                delayed_current_time = now - delay
+                
                 from_sample = self.stream_output_positions[signal_stream_name]
                 for sample in signal_stream_history.iterate_samples(from_sample, delayed_current_time):
                     self.stream_output_positions[signal_stream_name] += 1
